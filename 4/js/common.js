c974b9ab1496b0613ea327f1492bc1933cfd99dc
Boris <vboris@gmail.com>
2016-12-19 01:08:05 +0300

    Lesson2 Homework: slider attached, partially svg included

---
+++ 4/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // svg4everybody();
+    // $(".main-slider").owlCarousel({
+    //     loop: true,
+    //     margin: 10,
+    //     nav: true,
+    //     items: 1
+    // });
+    $('.single-item').slick();
+});
